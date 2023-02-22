06a97b4eaf15f410f55046aa428f23825ea66838
PeteAlej <97981494+PeteAlej@users.noreply.github.com>
2023-02-21 18:56:12 -0600

    Portfolio redesign
    Redesigned the portfolio, moving the top navbar to the side as it seemed a bit cleaner.

--- js/scripts.js
+++ js/scripts.js
@@ -19,26 +19,26 @@ function viewNavNames(obj) {
     let navElement = obj.id;
     switch(navElement){
         case "home":
-            element = document.getElementsByClassName("topbar-home");
-            arrow = document.getElementsByClassName("bubble1");
+            element = document.getElementsByClassName("home-cap");
+            arrow = document.getElementsByClassName("home-bubble");
             element[0].style.opacity = 1;
             arrow[0].style.opacity = 1;
             break;
         case "aboutme":
-            element = document.getElementsByClassName("topbar-aboutme");
-            arrow = document.getElementsByClassName("bubble2");
+            element = document.getElementsByClassName("aboutme-cap");
+            arrow = document.getElementsByClassName("aboutme-bubble");
             element[0].style.opacity = 1;
             arrow[0].style.opacity = 1;
             break;
         case "projects":
-            element = document.getElementsByClassName("topbar-projects");
-            arrow = document.getElementsByClassName("bubble3");
+            element = document.getElementsByClassName("projects-cap");
+            arrow = document.getElementsByClassName("projects-bubble");
             element[0].style.opacity = 1;
             arrow[0].style.opacity = 1;
             break;
         case "contact":
-            element = document.getElementsByClassName("topbar-contact");
-            arrow = document.getElementsByClassName("bubble4");
+            element = document.getElementsByClassName("contact-cap");
+            arrow = document.getElementsByClassName("contact-bubble");
             element[0].style.opacity = 1;
             arrow[0].style.opacity = 1;
             break;
@@ -49,26 +49,26 @@ function hideNavNames(obj) {
     let navElement = obj.id;
     switch(navElement){
         case "home":
-            element = document.getElementsByClassName("topbar-home");
-            arrow = document.getElementsByClassName("bubble1");
+            element = document.getElementsByClassName("home-cap");
+            arrow = document.getElementsByClassName("home-bubble");
             element[0].style.opacity = 0;
             arrow[0].style.opacity = 0;
             break;
         case "aboutme":
-            element = document.getElementsByClassName("topbar-aboutme");
-            arrow = document.getElementsByClassName("bubble2");
+            element = document.getElementsByClassName("aboutme-cap");
+            arrow = document.getElementsByClassName("aboutme-bubble");
             element[0].style.opacity = 0;
             arrow[0].style.opacity = 0;
             break;
         case "projects":
-            element = document.getElementsByClassName("topbar-projects");
-            arrow = document.getElementsByClassName("bubble3");
+            element = document.getElementsByClassName("projects-cap");
+            arrow = document.getElementsByClassName("projects-bubble");
             element[0].style.opacity = 0;
             arrow[0].style.opacity = 0;
             break;
         case "contact":
-            element = document.getElementsByClassName("topbar-contact");
-            arrow = document.getElementsByClassName("bubble4");
+            element = document.getElementsByClassName("contact-cap");
+            arrow = document.getElementsByClassName("contact-bubble");
             element[0].style.opacity = 0;
             arrow[0].style.opacity = 0;
             break;
